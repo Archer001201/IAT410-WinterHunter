0c510dfa594f343267343043c0f75477c62f990f
Archer <zhongchuqiao1201@gmail.com>
2024-01-17 16:29:02 -0800

    update throwing snowball method

---
+++ Winter Hunter/Assets/Scripts/Snowball/ThrowingSnowball.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+
+namespace Snowball
+{
+    public class ThrowingSnowball : MonoBehaviour
+    {
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.CompareTag("Player")) return;
+            Destroy(gameObject);
+        }
+    }
+}
