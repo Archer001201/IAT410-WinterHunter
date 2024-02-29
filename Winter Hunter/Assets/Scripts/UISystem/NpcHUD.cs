c7c3853b198c553c76203d69ca9eb689f1b62551
Archer <zhongchuqiao1201@gmail.com>
2024-02-28 16:58:46 -0800

    bug fixed

--- Winter Hunter/Assets/Scripts/UISystem/NpcHUD.cs
+++ Winter Hunter/Assets/Scripts/UISystem/NpcHUD.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -27,22 +28,17 @@ namespace UISystem
 
         protected virtual void Update()
         {
-            var worldPosition = trans.position + offset;
-            var screenPosition = _mainCamera.WorldToScreenPoint(worldPosition);
-
-            if (screenPosition.z > 0)
-            {
-                hudPanel.gameObject.SetActive(true);
-                hudPanel.position = screenPosition;
-            }
-            else
-            {
-                hudPanel.gameObject.SetActive(false);
-            }
-
             if (fillImage1 == null || fillImage2 == null) return;
             fillImage1.fillAmount = Mathf.Lerp(fillImage1.fillAmount, FillPercentage1, lerpSpeed * Time.deltaTime);
             fillImage2.fillAmount = Mathf.Lerp(fillImage2.fillAmount, FillPercentage2, lerpSpeed * Time.deltaTime);
+        }
+
+        private void FixedUpdate()
+        {
+            var worldPosition = trans.position + offset;
+            var screenPosition = _mainCamera.WorldToScreenPoint(worldPosition);
+            
+            hudPanel.position = screenPosition;
         }
     }
 }
