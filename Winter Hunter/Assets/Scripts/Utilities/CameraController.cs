3d0a90e4b3fad02c0da29ff3eeb8195d9a7bc83a
Archer <zhongchuqiao1201@gmail.com>
2024-02-29 12:18:59 -0800

    camera update

--- Winter Hunter/Assets/Scripts/Utilities/CameraController.cs
+++ Winter Hunter/Assets/Scripts/Utilities/CameraController.cs
@@ -7,10 +7,17 @@ namespace Utilities
     public class CameraController : MonoBehaviour
     {
         public CinemachineVirtualCamera virCamera;
+        // public Vector3 offset;
+        private Transform _playerTrans;
+        // private Transform _followTrans;
 
         private void Awake()
         {
-            virCamera.Follow = GameObject.FindWithTag("Player").transform;
+            _playerTrans = GameObject.FindWithTag("Player").transform;
+            // _followTrans.position = (_playerTrans.position += offset);
+            // // _followTrans.position += offset;
+            //
+            virCamera.Follow = _playerTrans;
         }
     }
 }
