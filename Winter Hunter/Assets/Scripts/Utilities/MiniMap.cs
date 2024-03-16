0ce488a8ad982922f867b1e5349807f718ecd8c5
Archer <zhongchuqiao1201@gmail.com>
2024-03-15 18:47:00 -0700

    Mini map

---
+++ Winter Hunter/Assets/Scripts/Utilities/MiniMap.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+namespace Utilities
+{
+    public class MiniMap : MonoBehaviour
+    {
+        public GameObject playerIndicator;
+
+        private void FixedUpdate()
+        {
+            
+        }
+    }
+}
